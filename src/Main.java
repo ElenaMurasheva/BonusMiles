bef752e7e0ebefcb416e77494503e02cf80c236a
Елена Мурашева <vacilika77@gmail.com>
2022-06-06 20:12:13 +0300

    Added Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,24 @@
 public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 510; // Цена билета
+
+        int bonus = 20; // Количество рублей для одной бонусной мили
+
+        int bonusMiles = ticketPrice/bonus; // Количество начисленных бонусных миль за купленный билет
+
+        System.out.println("Количество начисленных миль за купленный билет " + bonusMiles);
+
+
+
+
+
+
+
+    }
+
+
+
+
+
 }
