7d09e1a64bbe6fe56c949d1724c872ec3ee18b29
Елена Мурашева <vacilika77@gmail.com>
2022-06-07 21:07:42 +0300

    Revert "Modified Main.java"
    This reverts commit e387502f27deea994fd06a3f04aae8bd19fe10b7.

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,20 @@ public class Main {
 
         int bonus = 20; // Количество рублей для одной бонусной мили
 
-        int bonusMiles = ticketPrice / bonus; // Количество начисленных бонусных миль за купленный билет
+        int bonusMiles = ticketPrice/bonus; // Количество начисленных бонусных миль за купленный билет
 
         System.out.println("Количество начисленных миль за купленный билет " + bonusMiles);
 
 
+
+
+
+
+
     }
+
+
+
 
 
 }
